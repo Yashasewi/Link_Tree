967fd8b3992c92d77abca8858a74ca6104553544
Yashasewi <Yashasewisingh2668@gmail.com>
2023-06-25 19:35:59 +0530

    first commit

---
+++ Components/Effect.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useEffect } from "react";
+import styles from "./effect.module.scss";
+
+function Effect() {
+    useEffect(() => {
+        /* -- Glow effect -- */
+
+        const blob = document.getElementById(styles.blob);
+
+        window.onpointermove = (event) => {
+            const { clientX, clientY } = event;
+
+            blob.animate(
+                {
+                    left: `${clientX}px`,
+                    top: `${clientY}px`,
+                },
+                { duration: 3000, fill: "forwards" }
+            );
+        };
+
+        /* -- Text effect -- */
+
+        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        let interval = null;
+
+        document.querySelector("h1").onmouseover = (event) => {
+            let iteration = 0;
+
+            clearInterval(interval);
+
+            interval = setInterval(() => {
+                event.target.innerText = event.target.innerText
+                    .split("")
+                    .map((letter, index) => {
+                        if (index < iteration) {
+                            return event.target.dataset.value[index];
+                        }
+
+                        return letters[Math.floor(Math.random() * 26)];
+                    })
+                    .join("");
+
+                if (iteration >= event.target.dataset.value.length) {
+                    clearInterval(interval);
+                }
+
+                iteration += 1 / 3;
+            }, 30);
+        };
+    }, []);
+    return (
+        <div className={styles.effect}>
+            <div id={styles.blob}></div>
+            <div id={styles.blur}></div>
+        </div>
+    );
+}
+
+export default Effect;
